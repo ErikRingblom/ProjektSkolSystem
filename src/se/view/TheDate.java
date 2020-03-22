59c30917b8d49f1ecb24c2b1d16721532b4227d4
Rimanda18 <50340311+Rimanda18@users.noreply.github.com>
2020-03-22 23:40:25 +0100

    Initial repo

---
+++ src/se/view/TheDate.java
@@ -0,0 +1,21 @@
+package se.view;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author rimazivkovic
+ */
+public class TheDate {
+
+    private String theDate;
+
+    public String showDate() {
+
+        Date date = new Date();
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd  HH:mm");
+        this.theDate = s.format(date);
+        return theDate;
+    }
+}
